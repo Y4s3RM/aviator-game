bcc364f52d6b5b373235b3c013045249b4922d98
Y4s3RM <y4s3rma@gmail.com>
2025-10-05 02:35:53 +0330

    Update test-admin-system.js

--- backend/test-admin-system.js
+++ backend/test-admin-system.js
@@ -2,7 +2,7 @@
 
 const axios = require('axios');
 
-const API_BASE = 'http://localhost:3002/api';
+const API_BASE = 'https://aviator-api-xf7s.onrender.com/api';
 let adminToken = null;
 let testUserId = null;
 
